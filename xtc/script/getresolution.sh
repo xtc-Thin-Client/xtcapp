5cb784cb72b972502cd5c86b9877ace3d854644c
Stephan Eisler <stephan@eisler.de>
2021-02-11 17:07:31 +0100

    xtc 3

--- xtc/script/getresolution.sh
+++ xtc/script/getresolution.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 # get resolution
-
-OS="U"
-if (cat /etc/issue | grep -i "RASPBIAN" > /dev/null)
-then
-  OS="R"
-fi
-
-if [ "$OS" = "R" ]
-then
-  grep "^hdmi_mode=" /boot/config.txt | cut -d"=" -f2
-  
-else
-  xrandr --current | grep "*" | awk '{print $1}'
-fi
+xrandr --current | grep "*" | awk '{print $1}'
 exit 0
